b4ba36f7e940f57d107ee44479b2f06ba9b598d1
cssweb <cssweb@FrankmatoMacBook-Pro.local>
2014-03-26 09:17:16 +0800

    Initial Commit

---
+++ TestModule/NPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NPAppDelegate.h
+//  TestModule
+//
+//  Created by cssweb on 14-3-26.
+//  Copyright (c) 2014年 北京中软万维上海分公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestModule/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestModule
+//
+//  Created by cssweb on 14-3-26.
+//  Copyright (c) 2014年 北京中软万维上海分公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NPAppDelegate class]));
+    }
+}
